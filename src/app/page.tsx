751f8d2dbfcdc6e5d7516f7e5d1851898d20c77e
Irakli Khizanishvili <ikodado@gmail.com>
2023-04-15 12:38:30 -0700

    add github and linkedin links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+import Link from "next/link";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-1 bg-gray-100 flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+      <div className="bg-gray-100 flex flex-col flex-1 items-center justify-center gap-3">
+        <h1 className="text-xl md:text-2xl lg:text-4xl font-bold text-gray-800 text-center">
           🚧 Website Under Construction 🚧
         </h1>
+        <div className="flex gap-3">
+          <Link href="https://github.com/IrakliK">
+            <BsGithub size={50} color="#A0A0A0" />
+          </Link>
+          <Link href="https://www.linkedin.com/in/iraklikhizanishvili">
+            <BsLinkedin size={50} color="#A0A0A0" />
+          </Link>
+        </div>
       </div>
-      <div className="bg-gray-800 text-gray-300 text-center py-2">
+      <div className="bg-gray-800 text-gray-300 text-center py-2 text-xs">
         <p>
           &copy; {new Date().getFullYear()} Irakli Khizanishvili. All rights
           reserved.
