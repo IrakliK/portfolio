eb4e069443866d04c218e5f992ad952e1b80a79d
Irakli Khizanishvili <ikodado@gmail.com>
2023-04-22 10:38:01 -0700

    add resume link to website portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { SlEnvolopeLetter } from "react-icons/sl";
 
 export default function Home() {
   return (
@@ -8,12 +9,27 @@ export default function Home() {
         <h1 className="text-xl md:text-2xl lg:text-4xl font-bold text-gray-800 text-center">
           🚧 Website Under Construction 🚧
         </h1>
-        <div className="flex gap-3">
-          <Link href="https://github.com/IrakliK">
+        <div className="flex gap-3 text-gray-800">
+          <Link
+            href="https://github.com/IrakliK"
+            className="flex flex-col justify-center items-center gap-4"
+          >
+            <div className="flex font-bold">GITHUB</div>
             <BsGithub size={50} color="#A0A0A0" />
           </Link>
-          <Link href="https://www.linkedin.com/in/iraklikhizanishvili">
+          <Link
+            href="https://www.linkedin.com/in/iraklikhizanishvili"
+            className="flex flex-col justify-center items-center gap-4"
+          >
+            <div className="flex font-bold">LINKEDIN</div>
             <BsLinkedin size={50} color="#A0A0A0" />
+          </Link>
+          <Link
+            href="https://resume.io/r/8U06dz4yt"
+            className="flex flex-col justify-center items-center gap-4"
+          >
+            <div className="flex font-bold">RESUME</div>
+            <SlEnvolopeLetter size={50} color="#A0A0A0" />
           </Link>
         </div>
       </div>
