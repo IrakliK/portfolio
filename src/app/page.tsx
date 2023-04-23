ed6e6bd42c1f31fb20bc527302fee03754a5e193
Irakli Khizanishvili <ikodado@gmail.com>
2023-04-22 19:21:36 -0700

    update website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,49 @@
+import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { SlEnvolopeLetter } from "react-icons/sl";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen">
-      <div className="bg-gray-100 flex flex-col flex-1 items-center justify-center gap-3">
-        <h1 className="text-xl md:text-2xl lg:text-4xl font-bold text-gray-800 text-center">
-          🚧 Website Under Construction 🚧
-        </h1>
-        <div className="flex gap-3 text-gray-800">
+    <div className="flex flex-col h-screen text-white">
+      <div className="bg-[#1D1C4E] flex flex-col flex-1 items-center justify-center gap-6">
+        <div className="flex flex-col items-center">
+          <Image src="/avatar.jpg" alt="avatar" width={350} height={350} />
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="text-4xl font-bold">Irakli Khizanishvili</h1>
+            <span className="uppercase font-semibold text-xs">
+              Software Engineer
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-3">
           <Link
             href="https://github.com/IrakliK"
             className="flex flex-col justify-center items-center gap-4"
           >
             <div className="flex font-bold">GITHUB</div>
-            <BsGithub size={50} color="#A0A0A0" />
+            <BsGithub size={50} className="text-white hover:text-gray-400" />
           </Link>
           <Link
             href="https://www.linkedin.com/in/iraklikhizanishvili"
             className="flex flex-col justify-center items-center gap-4"
           >
             <div className="flex font-bold">LINKEDIN</div>
-            <BsLinkedin size={50} color="#A0A0A0" />
+            <BsLinkedin size={50} className="text-white hover:text-gray-400" />
           </Link>
           <Link
             href="https://resume.io/r/8U06dz4yt"
             className="flex flex-col justify-center items-center gap-4"
           >
             <div className="flex font-bold">RESUME</div>
-            <SlEnvolopeLetter size={50} color="#A0A0A0" />
+            <SlEnvolopeLetter
+              size={50}
+              className="text-white hover:text-gray-400"
+            />
           </Link>
         </div>
       </div>
-      <div className="bg-gray-800 text-gray-300 text-center py-2 text-xs">
+      <div className="bg-[#1D1C4E] text-center py-2 text-xs">
         <p>
           &copy; {new Date().getFullYear()} Irakli Khizanishvili. All rights
           reserved.
